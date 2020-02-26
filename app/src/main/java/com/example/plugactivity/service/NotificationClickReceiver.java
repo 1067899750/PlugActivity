698a4ed8c2f11c31cfcaca9b618f9af66c5fb21f
puyantao <puyantao123759>
2020-02-26 17:04:35 +0800

    11

--- app/src/main/java/com/example/plugactivity/service/NotificationClickReceiver.java
+++ app/src/main/java/com/example/plugactivity/service/NotificationClickReceiver.java
@@ -1,13 +1,18 @@
 package com.example.plugactivity.service;
 
+import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import com.example.plugactivity.MainActivity;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author puyantao
@@ -27,6 +32,43 @@ public class NotificationClickReceiver extends BroadcastReceiver {
         }
 
     }
+
+
+    private List<Activity> getActivitiesByApplication(Application application) {
+        List<Activity> list = new ArrayList<>();
+        try {
+            Class<Application> applicationClass = Application.class;
+            Field mLoadedApkField = applicationClass.getDeclaredField("mLoadedApk");
+            mLoadedApkField.setAccessible(true);
+            Object mLoadedApk = mLoadedApkField.get(application);
+            Class<?> mLoadedApkClass = mLoadedApk.getClass();
+            Field mActivityThreadField = mLoadedApkClass.getDeclaredField("mActivityThread");
+            mActivityThreadField.setAccessible(true);
+            Object mActivityThread = mActivityThreadField.get(mLoadedApk);
+            Class<?> mActivityThreadClass = mActivityThread.getClass();
+            Field mActivitiesField = mActivityThreadClass.getDeclaredField("mActivities");
+            mActivitiesField.setAccessible(true);
+            Object mActivities = mActivitiesField.get(mActivityThread);
+            // 注意这里一定写成Map，低版本这里用的是HashMap，高版本用的是ArrayMap
+            if (mActivities instanceof Map) {
+                @SuppressWarnings("unchecked")
+                Map<Object, Object> arrayMap = (Map<Object, Object>) mActivities;
+                for (Map.Entry<Object, Object> entry : arrayMap.entrySet()) {
+                    Object value = entry.getValue();
+                    Class<?> activityClientRecordClass = value.getClass();
+                    Field activityField = activityClientRecordClass.getDeclaredField("activity");
+                    activityField.setAccessible(true);
+                    Object o = activityField.get(value);
+                    list.add((Activity) o);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            list = null;
+        }
+        return list;
+    }
+
 
     /**
      *  判断APP是否运行
