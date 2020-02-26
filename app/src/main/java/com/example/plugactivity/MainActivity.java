ffb6ef4ea14e6bfc0fc02ace206e2ff46d7304e9
puyantao <puyantao123759>
2020-02-26 14:27:04 +0800

    11

--- app/src/main/java/com/example/plugactivity/MainActivity.java
+++ app/src/main/java/com/example/plugactivity/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.plugactivity.service.DownloadService;
 
+public class MainActivity extends AppCompatActivity {
+    public static final String APP_PACKAGE_NAME = "bsd_purang.apk";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DownloadService.startDownloadService(MainActivity.this,
+                "https://yanyangtian.purang.com/download/" + APP_PACKAGE_NAME,
+                APP_PACKAGE_NAME);
         new Handler(getMainLooper()).post(new Runnable() {
             @Override
             public void run() {
