d83ce65f532a16bca81f4322f3fcd6ce15dbc8a9
puyantao <puyantao123759>
2020-02-27 15:43:53 +0800

    11

---
+++ app/src/main/java/com/example/plugactivity/MyApplication.java
@@ -0,0 +1,32 @@
+package com.example.plugactivity;
+
+import android.app.Application;
+
+/**
+ * @author puyantao
+ * @describe
+ * @create 2020/2/27 15:36
+ */
+public class MyApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
